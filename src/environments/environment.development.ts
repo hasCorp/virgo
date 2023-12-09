68249720d43e9afdd88d398ead24ed32e081203f
Alyx <figments@outlook.com>
2023-12-09 08:54:16 -0800

    add basic supabase support

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://gvjqvxesnglnimjuozbp.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2anF2eGVzbmdsbmltanVvemJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDIwNjg2MzUsImV4cCI6MjAxNzY0NDYzNX0.0GBMCNP_EGAnpFZyMZTERtb7ggd6nLUMYzZucrbnPrs'
+};
